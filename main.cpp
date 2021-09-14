dc75003fef83adea37313359bdb0ca3e9dbe4d89
ChristianCARA <89394378+ChristianCARA@users.noreply.github.com>
2021-09-13 22:58:01 -0500

    pregunta 6 al 8

--- main.cpp
+++ main.cpp
@@ -6,97 +6,45 @@
 #include "operators.h"
 
 using namespace std;
-
 const int nElem = 20;
-
 int main()
 {
+  //pregunta 6
   TX vect[nElem] = {5,30,40, 7,80, 90, 3,25, 54,47, 
                   3, 6, 12, 8, 25, 27, 19, 83, 9, 0};
-  /*Queue<TX> queue;
-  for(auto x=0; x<10; x++)
-  {
-    queue.insert(vect[x]);
-    cout << queue << endl;
-  }
-  cout << endl;
-
-  for(auto x=0; x<10; x++)
-  {
-      auto dato = queue.pop();
-      cout << "dato removido: " << dato << endl;
-      cout << queue <<endl;
-  }*/
-
-  /*int x = 6;
-  int *px;
-  px = &x;
-  *px += 20;
-  cout << x << endl;*/
-
-  int z = 5;
-  LinkedList<TX> mylist;
-  for(auto x=0; x<10; x++)
+ //Insetar elemento en la lista
+ 
+ //pregunta 7
+ int mayor=0;
+   LinkedList<TX> mylist;
+  for(auto x=0; x<nElem; x++)
   {   mylist.insert_at_tail(vect[x]);
-      cout << mylist << " ... " << z << "hola " <<endl;
+      cout << mylist<<endl;
+      if(vect[x]>mayor){
+        mayor=vect[x];
+      }
   }
-  cout << endl;
+  cout <<"El numero mayor positivo es: "<<mayor<<endl;
 
+  //Pregunta 8
+  int acumulado=0;
+  T1 prom=0;
+  int cont=0;
+  LinkedList<TX> Promedio;
+    for(auto x=0; x<nElem; x++){
+
+      acumulado= acumulado + (vect[x]);    
+    }
+    
+    prom=acumulado/nElem;
+    cout<<"El promedio es: "<<prom<<endl;
+
+  }
+
+/* Intesar en la cabeza
   for(auto x=10; x< nElem; x++)
   {   mylist.insert_at_head(vect[x]);
       cout << mylist << endl;
   }
-  myList[10000] = 14;
+*/
 
-  iterator<TX> iter = mylist.begin();
-  for(; iter != mylist.end() ; iter++)
-  {
-      cout << *iter << endl;
-  }
-
-  /*while( !mylist.empty() )
-  {   
-      auto dato = mylist.PopHead();
-      cout << "dato removido: " << dato << endl;
-      cout << mylist <<endl;
-  }*/
-
-  try
-  {
-      auto dato = mylist.PopHead();
-      cout << "abc" << endl;
-  }
-  catch(int n) 
-  {
-    cout << "Excepcion atrapada ... puedes continuar !!!" <<endl;
-  }
-  catch(double n) 
-  {
-    cout << "Excepcion de doubles atrapada ... puedes continuar !!!" <<endl;
-  } 
-  catch(exception n) 
-  {
-    cout << "Excepcion generica atrapada ... puedes continuar !!!" <<endl;
-  }  
-  catch( ... )
-  {
-      cout << "todo lo demas atrapada ... puedes continuar !!!" <<endl;
-  }
-  cout << "Finalizando el programa ..." << endl;
-  
-  char c = 'A';
-  /*Stack<T1> stack;
-  Stack<T1> *pStack = new Stack<T1>;
-
-  auto w = "hola";
-  auto x = 0; 
-  for( x = 0 ; x < nElem ; x++ )
-     stack.push(x+x);
-  
-  for( x = 0 ; x < nElem ; x++ )
-  {
-    auto y = stack.pop();
-    cout << y << endl;
-  }
-  */
-}
